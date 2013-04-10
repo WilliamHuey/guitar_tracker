f2e2d155c4a33f3631771ea8cd0da09326ef1e10
WilliamHuey <lliw1236@yahoo.com>
2013-04-10 12:10:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuitarTracker::Application.config.secret_token = '1c33f6114f7384ae80da402bac4890aac24b34a2f933049ea49c3e2ededfa4c4d1968ba24ec99cb58e176aa19d4265101ae4aeceab9cd8eb428f9a82b0795971'
