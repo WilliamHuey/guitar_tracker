8ec4be59ffade57fc31f052159bac84977ca8aec
WilliamHuey <lliw1236@yahoo.com>
2013-04-11 16:34:11 -0700

    change format of validates in guitar.rb

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -4,13 +4,13 @@ class Guitar < ActiveRecord::Base
   time = Time.new
   current_year = time.year
 
-  validates_presence_of :brand, :with => /\A\Z/
-  validates_presence_of :modelName, :with => /\A\Z/
-  validates_presence_of :color, :with => /\A\Z/
+  validates :brand, presence: { with: /\A\Z/ }
+  validates :modelName, presence: { with: /\A\Z/ }
+  validates :color, presence: { with: /\A\Z/ }
 
-  validates :year, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: current_year}
-  validates :weight, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :frets, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :strings, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :year, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: current_year }
+  validates :weight, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :frets, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :strings, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
 end
--- spec/models/guitar_spec.rb
+++ spec/models/guitar_spec.rb
@@ -1,40 +1,41 @@
 require 'spec_helper'
 
 describe Guitar do
+
   before do
     @guitar = Guitar.new()
   end
+
   subject { @guitar }
 
   it { should allow_value("a brand name").for(:brand) }
   it { should allow_value("name of a model").for(:modelName) }
   it { should allow_value("some color").for(:color) }
 
-  it { should_not allow_value("not year").for(:year) }
-  it { should_not allow_value("not a weight").for(:weight) }
-  it { should_not allow_value("not number of frets").for(:frets) }
-  it { should_not allow_value("not a number of strings").for(:strings) }
-
+  it { should allow_value(1995).for (:year) }
   it { should_not allow_value(-3).for (:year) }
   it { should_not allow_value(0).for (:year) }
   it { should_not allow_value(1997.5).for (:year) }
-  it { should_not allow_value(999999).for (:year)}
-  it { should allow_value(1995).for (:year) }
+  it { should_not allow_value(999999).for (:year) }
+  it { should_not allow_value("not year").for(:year) }
 
-  it { should allow_value(3).for(:weight)}
+  it { should allow_value(3).for(:weight) }
   it { should_not allow_value(-3).for (:weight) }
   it { should_not allow_value(3.5).for (:weight) }
   it { should_not allow_value(0).for (:weight) }
+  it { should_not allow_value("not a weight").for(:weight) }
 
-  it { should allow_value(3).for(:frets)}
+  it { should allow_value(3).for(:frets) }
   it { should_not allow_value(-3).for (:frets) }
   it { should_not allow_value(3.5).for (:frets) }
   it { should_not allow_value(0).for (:frets) }
+  it { should_not allow_value("not number of frets").for(:frets) }
 
-  it { should allow_value(3).for(:strings)}
+  it { should allow_value(3).for(:strings) }
   it { should_not allow_value(-3).for (:strings) }
-  it { should_not allow_value(-3).for (:strings) }
+  it { should_not allow_value(3.5).for (:strings) }
   it { should_not allow_value(0).for (:strings) }
+  it { should_not allow_value("not a number of strings").for(:strings) }
 
 
 end
