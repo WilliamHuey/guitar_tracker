b83cae4fe453ec3a0225aa9c83f3c888157587e4
WilliamHuey <lliw1236@yahoo.com>
2013-04-11 16:23:22 -0700

    add model attribute type validations and tests

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,3 +1,16 @@
 class Guitar < ActiveRecord::Base
   attr_accessible :brand, :color, :frets, :modelName, :strings, :weight, :year
+
+  time = Time.new
+  current_year = time.year
+
+  validates_presence_of :brand, :with => /\A\Z/
+  validates_presence_of :modelName, :with => /\A\Z/
+  validates_presence_of :color, :with => /\A\Z/
+
+  validates :year, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: current_year}
+  validates :weight, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :frets, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :strings, presence: true, numericality: {only_integer: true, greater_than: 0}
+
 end
--- spec/models/guitar_spec.rb
+++ spec/models/guitar_spec.rb
@@ -6,12 +6,31 @@ describe Guitar do
   end
   subject { @guitar }
 
-  it { should allow_value(nil).for(:brand) }
-  it { should allow_value(nil).for(:modelName) }
-  it { should allow_value(nil).for(:year) }
-  it { should allow_value(nil).for(:color) }
-  it { should allow_value(nil).for(:weight) }
-  it { should allow_value(nil).for(:frets) }
-  it { should allow_value(nil).for(:strings) }
+  it { should allow_value("a brand name").for(:brand) }
+  it { should allow_value("name of a model").for(:modelName) }
+  it { should allow_value("some color").for(:color) }
+
+  it { should_not allow_value("not year").for(:year) }
+  it { should_not allow_value("not a weight").for(:weight) }
+  it { should_not allow_value("not number of frets").for(:frets) }
+  it { should_not allow_value("not a number of strings").for(:strings) }
+
+  it { should_not allow_value(-3).for (:year) }
+  it { should_not allow_value(0).for (:year) }
+  it { should_not allow_value(999999).for (:year)}
+  it { should allow_value(1995).for (:year) }
+
+  it { should allow_value(3).for(:weight)}
+  it { should_not allow_value(-3).for (:year) }
+  it { should_not allow_value(0).for (:weight) }
+
+  it { should allow_value(3).for(:frets)}
+  it { should_not allow_value(-3).for (:frets) }
+  it { should_not allow_value(0).for (:frets) }
+
+  it { should allow_value(3).for(:strings)}
+  it { should_not allow_value(-3).for (:strings) }
+  it { should_not allow_value(0).for (:strings) }
+
 
 end
