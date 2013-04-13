8ece4210a841294122543b11dc4c1b5aaff62227
WilliamHuey <lliw1236@yahoo.com>
2013-04-12 19:24:50 -0700

    ember index redirects to guitars

--- app/assets/javascripts/guitars.js
+++ app/assets/javascripts/guitars.js
@@ -1,12 +1,28 @@
-GuitarTracker = Ember.Application.create();
+GuitarTracker = Ember.Application.create({
+    rootElement: '#right-panel-unordered-list'
+});
+
+GuitarTracker.IndexRoute = Ember.Route.extend({
+    redirect: function() {
+        this.transitionTo('guitars');
+    }
+});
 
 GuitarTracker.Store = DS.Store.extend({
     revision: 12,
     adapter: 'DS.FixtureAdapter'
 });
 
-GuitarTracker.Router.map(function(){
-   this.resource('about');
+GuitarTracker.Router.map(function () {
+    this.resource('guitars');
+    this.resource('about');
+});
+
+GuitarTracker.GuitarsRoute = Ember.Route.extend({
+   model: function() {
+
+       return GuitarTracker.Guitar.find();
+   }
 });
 
 GuitarTracker.Guitar = DS.Model.extend({
@@ -18,3 +34,27 @@ GuitarTracker.Guitar = DS.Model.extend({
     frets: DS.attr('integer'),
     strings: DS.attr('integer')
 });
+
+GuitarTracker.Guitar.FIXTURES = [
+    {
+        id: 1,
+        brand: 'Gibson',
+        modelName: 'the name of model is',
+        year: 2004,
+        color: 'rainbow',
+        weight: 35.6,
+        frets: 6,
+        strings: 7
+    },
+    {
+        id: 2,
+        brand: 'Aria',
+        modelName: 'a name for a model',
+        year: 2004,
+        color: 'rainbow',
+        weight: 35.6,
+        frets: 6,
+        strings: 7
+
+    }
+];
